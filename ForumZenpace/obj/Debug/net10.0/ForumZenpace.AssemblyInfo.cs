dfee59724e387ebd5374de9fc6c0d3a609d96bca
Tuan Tran Quang <tuantqtb01555@example.com>
2026-03-19 21:23:37 +0700

    Update rjsmrazor.dswa.cache.json to reflect new global properties and input hashes

--- ForumZenpace/obj/Debug/net10.0/ForumZenpace.AssemblyInfo.cs
+++ ForumZenpace/obj/Debug/net10.0/ForumZenpace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForumZenpace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71921a85b07a0b821e9665d91319400a06381d1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9e9ec601d6ed462ab4ed4a536f73803f7fd505")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForumZenpace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForumZenpace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
